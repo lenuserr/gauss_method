74644fe92dc67159cfa9de19c564c0e027f6ff95
lenuserr <126238556+lenuserr@users.noreply.github.com>
2023-10-11 18:45:58 +0300

    Update r.cpp

--- r.cpp
+++ r.cpp
@@ -2,10 +2,8 @@
 #include <cmath>
 #include "inc.h"
 
-double r1_eval(const std::vector<double>& matrix, const std::vector<double>& x,
+double r1_eval(int n, const std::vector<double>& matrix, const std::vector<double>& x,
     const std::vector<double>& b, std::vector<double>* c, std::vector<double>* d) {
-    
-    int n = x.size();
     
     matr_prod(n, n, 1, matrix, x, c);
     subtract_vector(n, *c, b, d);
@@ -13,8 +11,8 @@ double r1_eval(const std::vector<double>& matrix, const std::vector<double>& x,
     return vector_norm(*d) / vector_norm(b);
 }
 
-double r2_eval(const std::vector<double>& x) {
-    int n = x.size();
+double r2_eval(int n, const std::vector<double>& x) {
+    
     double r2 = 0;
     for (int i = 1; i <= n; ++i) {
         r2 += std::fabs(x[i - 1] - (i % 2));
